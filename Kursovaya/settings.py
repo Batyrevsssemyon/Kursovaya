1e557fc46df19e33d763334dbbbaca3dc2493dcd
Semmma <55916069+Batyrevsssemyon@users.noreply.github.com>
2024-01-31 09:26:40 +0700

    Update settings.py

--- Kursovaya/settings.py
+++ Kursovaya/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,7 +83,7 @@ DATABASES = {
     }
 }
 
-# postgres://kursovaya_postgres_user:lAPjD5MlfLn9tSBRF55G1yxLSGfUVBGw@dpg-cmsqqha1hbls73cku9h0-a.oregon-postgres.render.com/kursovaya_postgres
+DATABASES['default'] = dj_database_url.parse('postgres://kursovaya_postgres_user:lAPjD5MlfLn9tSBRF55G1yxLSGfUVBGw@dpg-cmsqqha1hbls73cku9h0-a.oregon-postgres.render.com/kursovaya_postgres')
 
 
 # Password validation
@@ -124,6 +125,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
